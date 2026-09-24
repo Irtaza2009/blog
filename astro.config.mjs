158cc92508ed55b4fc5eae43e8c68390f74a3514
Irtaza2009 <irtazanaqvi05@gmail.com>
2026-09-24 23:03:24 +0500

    added read time for each post

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+import { remarkReadingTime } from "./src/utils/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://irtaza.xyz',
-	base: '/blog/',
-	integrations: [mdx(), sitemap()],
-	markdown: {},
+  site: "https://irtaza.xyz",
+  base: "/blog/",
+  integrations: [mdx(), sitemap()],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
 });
